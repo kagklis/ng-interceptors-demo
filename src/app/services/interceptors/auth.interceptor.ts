0a050335c54b36ef784a5a8080031bbfc4e5c382
kagklis <kagklis@gmail.com>
2022-07-18 01:35:57 +0300

    Added example of HttpContextToken

--- src/app/services/interceptors/auth.interceptor.ts
+++ src/app/services/interceptors/auth.interceptor.ts
@@ -1,21 +1,48 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+   HttpInterceptor,
+   HttpEvent,
+   HttpHandler,
+   HttpRequest,
+   HttpContextToken,
+   HttpResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { AuthService } from '../auth.service';
 
+export const LOGIN_REQUEST = new HttpContextToken(() => false);
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
    constructor(private authService: AuthService) {}
 
-   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+   intercept(
+      req: HttpRequest<any>,
+      next: HttpHandler
+   ): Observable<HttpEvent<any>> {
+      // We don't have a backend server, so for the login request
+      // we mock the response based on the passed context.
+      if (req.context.get(LOGIN_REQUEST)) {
+         return this.mockTokenResponse();
+      }
+
       const modifiedRequest = req.clone({
          setHeaders: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ' + ${this.authService.token}`
-         }
+            Authorization: `Bearer ' + ${this.authService.token}`,
+         },
       });
       console.log('[AuthInterceptor] - Setting request headers...');
       return next.handle(modifiedRequest);
+   }
+
+   private mockTokenResponse(): Observable<HttpEvent<any>> {
+      return of(
+         new HttpResponse({
+            body: {
+               token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ',
+            },
+         })
+      );
    }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,37 +1,40 @@
-import { HttpBackend, HttpClient } from '@angular/common/http';
+import { HttpBackend, HttpClient, HttpContext } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { map, Observable, of, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { CacheService } from './cache.service';
+import { LOGIN_REQUEST } from './interceptors/auth.interceptor';
 
 @Injectable({
    providedIn: 'root',
 })
 export class LoginService {
    public isLoggedIn$ = this.authService.token$.pipe(map((token) => !!token));
-   private httpClient: HttpClient;
 
    constructor(
       handler: HttpBackend,
+      private httpClient: HttpClient,
       private cacheService: CacheService,
       private authService: AuthService,
       private router: Router
    ) {
-      this.httpClient = new HttpClient(handler);
+      // If we wanted to bypass the entire chain of interceptors
+      // we would use the following instance instead.
+      // this.httpClient = new HttpClient(handler);
    }
 
    public login(creds: Credentials): Observable<any> {
-      // Normally, we'd have something like this.httpClient.post<any>('/login', creds)
-      // To keep this demo as simple as possible, we're going to return a fixed jwt token
-      return of({
-         token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ',
-      }).pipe(
-         tap((response) => {
-            this.authService.setToken(response.token);
-            this.router.navigate(['home']);
+      return this.httpClient
+         .post<any>('/login', creds, {
+            context: new HttpContext().set(LOGIN_REQUEST, true),
          })
-      );
+         .pipe(
+            tap((response) => {
+               this.authService.setToken(response.token);
+               this.router.navigate(['home']);
+            })
+         );
    }
 
    public logout(): void {
