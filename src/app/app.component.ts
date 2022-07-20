2544dfe3cf77ce100de45dccd4952dd9a54c78c9
kagklis <kagklis@gmail.com>
2022-07-20 23:32:31 +0300

    Removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthService } from './services/auth.service';
 import { LoginService } from './services/login.service';
 
 @Component({
